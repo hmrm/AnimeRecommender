b273dc21f149e1cc09e5706015771762230a177e
hmrm <hmrmaxwell@gmail.com>
2012-11-02 22:11:29 -0500

    whoops, using the same name for two projects is a bad idea

--- app/models/rating.rb
+++
@@ -1,3 +0,0 @@
-class Rating < ActiveRecord::Base
-  attr_accessible :anime_id, :rating, :user_id
-end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-AnimeRecommender::Application.config.secret_token = 'a87a94972b3226041e6982c18eb0d2c181dbd31a0aca24127fe3ed4aabe8441599fea91255dddafa2f625ade6b1af5483baaf41dbf20a19704848962e6fda7a4'
