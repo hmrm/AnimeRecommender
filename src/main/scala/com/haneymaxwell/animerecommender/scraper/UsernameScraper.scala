79857913357f77feb61ce7145f4ec2f9052fff57
Haney Maxwell <hmrmaxwell@gmail.com>
2014-03-16 13:25:48 -0700

    adding parsing

--- src/main/scala/com/haneymaxwell/animerecommender/scraper/UsernameScraper.scala
+++ src/main/scala/com/haneymaxwell/animerecommender/scraper/UsernameScraper.scala
@@ -24,6 +24,20 @@ object UsernameScraper {
 
   case class GenerateNamesResult(femaleNames: Set[Username], maleNames: Set[Username])
 
+  def getResults(gender: Gender, index: Int): Future[String] = Future {
+    lazy val scrape = new Scraper
+    try {
+      scrape source genUrl(gender, index)
+    } finally {
+      scrape.cleanup()
+    }
+  }
+
+  def getNames(gender: Gender, index: Int): Future[Set[Username]] = {
+    import scala.util.matching.Regex.Match
+    getResults(gender, index) map (NameExtractor.findAllMatchIn(_).toSet.map((m: Match) => Username(m.group("name"))))
+  }
+
   def generateNames(lastSuccessful: GenerateNamesResult = GenerateNamesResult(Set(), Set())): (Future[Unit], BlockingQueue[Username]) = {
     lazy val queue = new ArrayBlockingQueue[Username](100)
     def recur(lastSuccessful: GenerateNamesResult): Future[Unit] = {
@@ -53,25 +67,52 @@ object UsernameScraper {
     (recur(lastSuccessful), queue)
   }
 
-  def getResults(gender: Gender, index: Int): Future[String] = Future {
-    lazy val scraper = new Scraper
-    try {
-      scraper(genUrl(gender, index))
-    } finally {
-      scraper.cleanup()
-    }
+  def genUrl(username: Username): String =
+    s"http://myanimelist.net/malappinfo.php?u=${username.get}&status=all&type=anime"
+
+  def processName(name: Username): Future[String] = {
+    lazy val scrape = new Scraper
+    lazy val res = Future(scrape text genUrl(name))
+    res onComplete { _ => scrape.cleanup() }
+    res map { str => str.split('\n').mkString("") } // I am not entirely sure why this is necessary, but it is
   }
 
-  def getNames(gender: Gender, index: Int): Future[Set[Username]] = {
-    import scala.util.matching.Regex.Match
-    getResults(gender, index) map (NameExtractor.findAllMatchIn(_).toSet.map((m: Match) => Username(m.group("name"))))
+  // Note: it would be nicer just to parse this into XML, but it looks like it isn't adequately standards conforming
+  def genXmlRegex(tag: String) = s"""(?<=<$tag>)((?!</$tag>).)*(?=</$tag>)""".r
+
+  lazy val GetSeries = genXmlRegex("anime")
+  lazy val GetName   = genXmlRegex("series_title")
+  lazy val GetAID    = genXmlRegex("series_animedb_id")
+  lazy val GetRating = genXmlRegex("my_score")
+
+  case class Rating(get: Int)
+  case class AID(get: Int)
+  case class SeriesName(get: String)
+
+  def processData(data: String): (Map[AID, SeriesName], Map[AID, Rating]) = {
+    lazy val res: Seq[((AID, SeriesName), (AID, Rating))] = (GetSeries.findAllIn(data).toSeq map { case text =>
+      for {
+        name   <- GetName  .findFirstIn(text)
+        aid    <- GetAID   .findFirstIn(text)
+        rating <- GetRating.findFirstIn(text)
+      } yield ((AID(aid.toInt) -> SeriesName(name)), (AID(aid.toInt) -> Rating(rating.toInt)))
+    }).flatten
+
+    (res.map(x => x._1).toMap, res.map(x => x._2).toMap)
   }
+
 }
 
-class Scraper extends Chrome with (String => String) {
-  def apply(url: String) = {
+class Scraper extends Chrome {
+  def source(url: String) = {
     go to url
     pageSource
+  }
+
+  def text(url: String) = {
+    import org.openqa.selenium.By
+    go to url
+    webDriver.findElement(By.tagName("body")).getText
   }
 
   def cleanup() = close()
