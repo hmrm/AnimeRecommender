5fda42ec1438bca8164cc4ccae5b8e36699c8154
Haney Maxwell <hmrmaxwell@gmail.com>
2014-03-30 21:11:35 -0700

    Logging to logfile

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-    echo "Starting up"
+    echo "Starting up" | tee -a logfile
     sbt run | tee -a logfile
-    echo "Stopped, restarting"
+    echo "Stopped, restarting" | tee -a logfile
 done;
