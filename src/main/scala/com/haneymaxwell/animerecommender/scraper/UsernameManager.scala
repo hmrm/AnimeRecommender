673644df8c48ae44fed99b334714de26cbc252e7
Haney Maxwell <hmrmaxwell@gmail.com>
2014-08-24 19:53:41 -0700

    WIP migrate to actors

--- src/main/scala/com/haneymaxwell/animerecommender/scraper/UsernameManager.scala
+++ src/main/scala/com/haneymaxwell/animerecommender/scraper/UsernameManager.scala
@@ -1,83 +1,51 @@
 package com.haneymaxwell.animerecommender.scraper
 
+import java.util.concurrent.Executors
+
 import Data._
+import akka.actor.{ActorRef, Actor}
 import scala.concurrent._
 import com.haneymaxwell.animerecommender.Util._
-import scala.concurrent.ExecutionContext.Implicits.global
+import duration._
+import akka.pattern.pipe
 
 object UsernameManager {
+  implicit val ec = ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
+
 
   import java.util.concurrent.atomic.AtomicInteger
 
   lazy val blocksInFlight: AtomicInteger = new AtomicInteger(0)
 
-  import com.haneymaxwell.animerecommender.scraper.QueueUtils.CompletableQueue
-
-  def updateLeastRecent(queue: CompletableQueue[(Username, Gender)], n: Int): Future[Unit] = Future {
-    println(s"Updating $n least recently updated users")
-    DB.usernamesSortedByRecentness(n) foreach { user => blocking(queue.put(user)) }
-  } escalate
-
-  case class Block(get: Int)
-
-  def getLastBlockStart(scraper: DriverManager, gender: Gender): Future[Int] = {
-    import scala.util.matching.Regex
-    lazy val lastPageRegex: Regex = """(?<=show=)[0-9]*(?=">Last)""".r
-    lazy val page: Future[String] =  UsernameScraper.getResults(gender = gender, index = 0, scrape = scraper)
-    page map { p =>
-      val res = lastPageRegex.findFirstIn(p).get.toInt
-      println(s"Detected last username block for gender $gender at $res")
-      res
-    }
-  }
-
-  def generateBlocks(last: Int,
-                     gender: Gender,
-                     queue: CompletableQueue[(Block, Gender)]): Future[Unit] = Future {
-    (for {
-      i <- Range.inclusive(0, last, 25)
-      if !DB.blockPresent((Block(i), gender))
-    } yield (Block(i), gender)) foreach { bg =>
-      DB.addBlock(bg)
-      blocking(queue.put(bg))
-    }
-  }
-
-  def consumeBlock(blocks: CompletableQueue[(Block, Gender)],
-                   scraper: DriverManager,
-                   usernameQueue: CompletableQueue[(Username, Gender)]): Future[Unit] = {
-    Future(blocking(blocks.take())) flatMap { case (block, gender) =>
-      println(s"Generating names for $block $gender")
-      println(s"${blocksInFlight.incrementAndGet()} blocks in flight")
-      blocking(UsernameScraper.generateNames(scraper, usernameQueue, gender, block.get)) map { _ =>
-        println(s"Finished generating names for $block $gender, ${blocksInFlight.decrementAndGet()} blocks in flight")
-        DB.processBlock(block, gender)
+  class UsernameScrapingActor(prefix: String, gender: Gender, scraper: DriverManager, next: ActorRef) extends Actor {
+    case class LastBlock(i: Int)
+    case object Done
+    def getLastBlockStart: Future[LastBlock] = {
+      import scala.util.matching.Regex
+      lazy val lastPageRegex: Regex = """(?<=show=)[0-9]*(?=">Last)""".r
+      lazy val page: Future[String] =  UsernameScraper.getResults(gender, 0, scraper, prefix)
+      page map { p =>
+        val res = lastPageRegex.findFirstIn(p).getOrElse("0").toInt
+        println(s"Detected last username block for gender $gender at $res")
+        LastBlock(res)
       }
-    } flatMap(_ => consumeBlock(blocks, scraper, usernameQueue))
-  }
-
-  def updateLeastRecentBlocks(blockQueue: CompletableQueue[(Block, Gender)],
-                              n: Int,
-                              scraper: DriverManager): Future[Unit] = Future {
-    println(s"Updating $n old blocks")
-    DB.blocksSortedByRecentness(n) foreach blocking(blockQueue.put(_))
-  }
-
-  def runBlocks(scraper: DriverManager,
-                usernameQueue: CompletableQueue[(Username, Gender)],
-                blockQueue: CompletableQueue[(Block, Gender)],
-                gender: Gender,
-                parallelism: Int): Unit = {
-    def generateBlocksFromLast: Future[Unit] = getLastBlockStart(scraper, gender) flatMap { last => generateBlocks(last, gender, blockQueue) }
-
-    def alternateGenerate(): Unit       = generateBlocksFromLast.escalate                             onSuccess {case x => alternateUpdate()}
-    def alternateUpdate(): Unit         = updateLeastRecentBlocks(blockQueue, 1000, scraper).escalate onSuccess {case x => alternateUsernameUpdate()}
-    def alternateUsernameUpdate(): Unit = updateLeastRecent(usernameQueue, 10000).escalate            onSuccess {case x => alternateGenerate()}
-
-    Range(0, parallelism).par foreach { _ =>
-      blocking(consumeBlock(blockQueue, scraper, usernameQueue))
     }
 
-    alternateGenerate()
+    def receive: Unit = {
+      case LastBlock(i) => {
+        Range(0, i + 1, 25) foreach (self ! _)
+        self ! Done
+      }
+      case j: Int       => UsernameScraper.generateNames(scraper, gender, prefix, j, next)
+      case Done         => context.system.scheduler.scheduleOnce(30.days)(getLastBlockStart pipeTo self)
+    }
+
+    getLastBlockStart pipeTo self
   }
+
+  val querychars = ('a' to 'z').union('0' to '9').union(Seq('-', '_'))
+  val prefixes = for {
+    ch1 <- querychars
+    ch2 <- querychars
+  } yield s"$ch1$ch2"
 }
--- src/main/scala/com/haneymaxwell/animerecommender/scraper/UsernameScraper.scala
+++ src/main/scala/com/haneymaxwell/animerecommender/scraper/UsernameScraper.scala
@@ -2,11 +2,14 @@ package com.haneymaxwell.animerecommender.scraper
 
 import java.util.concurrent.Executors
 
+import akka.actor.ActorRef
+import com.typesafe.scalalogging.slf4j.LazyLogging
+
 import Predef.{any2stringadd => _, _}
 import scala.concurrent.{ExecutionContext, blocking, Future}
 import scala.util.matching.Regex
 
-object UsernameScraper {
+object UsernameScraper extends LazyLogging {
   implicit lazy val ec = ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
 
   import Data._
@@ -14,24 +17,24 @@ object UsernameScraper {
 
   lazy val BaseUrl = "http://myanimelist.net/users.php?q="
 
-  def genUrl(gender: Gender, index: Long) = s"$BaseUrl&g=${gender.toInt}&show=$index"
+  def genUrl(gender: Gender, index: Long, prefix: String) = s"$BaseUrl$prefix&g=${gender.toInt}&show=$index"
 
   lazy val NameExtractor: Regex = new Regex( """profile/([^"]+)">\1</a></div>""", "name")
 
   case class GenerateNamesResult(femaleNames: Set[Username], maleNames: Set[Username])
 
-  def getResults(gender: Gender, index: Long, scrape: DriverManager): Future[String] =
-    scrape source genUrl(gender, index)
+  def getResults(gender: Gender, index: Long, scrape: DriverManager, prefix: String): Future[String] =
+    scrape source genUrl(gender, index, prefix)
 
-  def getNames(gender: Gender, index: Long, scrape: DriverManager): Future[Set[Username]] = {
+  def getNames(gender: Gender, index: Long, scrape: DriverManager, prefix: String): Future[Set[Username]] = {
     import scala.util.matching.Regex.Match
-    getResults(gender, index, scrape) map (NameExtractor.findAllMatchIn(_).toSet.map((m: Match) => Username(m.group("name"))))
+    getResults(gender, index, scrape, prefix) map (NameExtractor.findAllMatchIn(_).toSet.map((m: Match) => Username(m.group("name"))))
   }
 
-  def generateNames(scrape: DriverManager, queue: CompletableQueue[(Username, Gender)], gender: Gender, start: Int) = {
-    println(s"Scraping for new usernames for gender $gender starting from $start")
+  def generateNames(scrape: DriverManager, gender: Gender, prefix: String, offset: Int, sendTo: ActorRef) = {
+    println(s"Scraping for new usernames for gender $gender with prefix $prefix, from $offset")
 
-    lazy val result: Future[Set[Username]] = getNames(gender, start, scrape)
+    lazy val result: Future[Set[Username]] = getNames(gender, offset, scrape, prefix)
 
     def putIfAbsent(username: Username) = {
       if (DB.usernamePresent(username)) {
@@ -39,7 +42,7 @@ object UsernameScraper {
       } else {
         println(s"Scraped username $username which is a new username, enqueueing for processing")
         DB.addUsername(username, gender)
-        blocking(queue.put((username, gender)))
+        sendTo ! username
         Metrics.newUsernameProcessed.incrementAndGet()
       }
     }
